55a4a8f2f826e0d8310619b4fb581a87b388a8a6
iamKhang <iamhoangkhang@icloud.com>
2024-08-04 14:43:14 +0700

    TextInput

--- App.tsx
+++ App.tsx
@@ -1,24 +1,52 @@
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [khang, setKhang] = useState({
-    name: "Khang",
-    age: 20,
-  });
+  const [name, setName] = useState<string>("N/A");
+  const [age, setAge] = useState<number>(0);
+
   return (
     <View style={styles.container}>
+      <Text>Name:</Text>
+      <TextInput
+        style={{
+          width: 200,
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+        onChangeText={(value) => {
+          setName(value);
+        }}
+        autoCapitalize="words"
+        multiline={true}
+        numberOfLines={4}
+      ></TextInput>
+
+      <Text>Age:</Text>
+      <TextInput
+        style={{
+          width: 200,
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+        onChangeText={(value) => {
+          setAge(+value);
+        }}
+        keyboardType="numeric"
+        maxLength={2}
+      ></TextInput>
+
       <Text>
-          Lê Hoàng Khang is {khang.age} years old.
+        {name} is {age} years old.
       </Text>
-      <Button title="Happy birthday!"onPress={
-        () => {
-          setKhang({
-            ...khang,
-            age: khang.age + 1,
-          });
-        }
-      }/>
+      <Button
+        title="Happy birthday!"
+        onPress={() => {
+          setAge(age + 1);
+        }}
+      />
     </View>
   );
 }

9bbe8e7ac0cf440099100dfec759835754874a5f
iamKhang <iamhoangkhang@icloud.com>
2024-08-04 10:56:28 +0700

    Style

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        {/* Component con không kế thừa CSS từ cha (container) ngoại trừ các thuộc tính mặc định */}
+        <Text style={styles.header}>Hello World</Text>
+      </View>
     </View>
   );
 }
 
+// CSS in JS
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  demoCSS: {
+    color: "violet",
+    fontSize: 60,
+    borderColor: "green",
+    borderWidth: 1,
+  },
+
+  header: {
+    fontSize: 30,
+    color: "red",
+    fontWeight: "bold",
   },
 });
